6d8efb69755ceeebd0bd11b1268b9e724e598e89
Your Name <you@example.com>
2021-02-01 15:04:40 -0500

    added minute variable

--- js/basics-variables-1.js
+++ js/basics-variables-1.js
@@ -5,3 +5,13 @@ const hoursPerDay = 24;
 console.log(hoursPerDay)
 
 console.log(daysPerYear);
+
+const hoursPerYear = daysPerYear * hoursPerDay;
+
+console.log(hoursPerYear) 
+
+const minutesPerHour = 60;
+
+const minutesPerDay = minutesPerHour * hoursPerDay;
+
+console.log(minutesPerDay)
