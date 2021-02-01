f50581dccd6793ba8ce8a643a25eed98e8cae12e
Your Name <you@example.com>
2021-02-01 16:22:41 -0500

    added random number variable

--- js/basics-variables-1.js
+++ js/basics-variables-1.js
@@ -36,10 +36,20 @@ const circle = prompt("Input a number 1-10")
 
 console.log(Math.pow(circle, 2))
 
-*/
+
 
 const width = prompt('Give me a number value between 1 - 10')
 const height = prompt('Give me another number value between 10 - 20')
 
  const rectangle = height * width;
  console.log(rectangle)
+*/
+const num = prompt("Guess a number 1-10")
+ const randomNumber = Math.floor(Math.random());
+ console.log(randomNumber)
+if( num === randomNumber) {
+    console.log("Congratulations!") 
+
+} else{
+    console.log("Nope")
+}
