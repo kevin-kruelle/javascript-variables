109a79e32afb09e95887478673ac5359133c60f5
Kevin Kruelle <kkruelle2@gmail.com>
2021-02-01 15:13:31 -0500

    added more variables

--- js/basics-variables-1.js
+++ js/basics-variables-1.js
@@ -15,3 +15,15 @@ const minutesPerHour = 60;
 const minutesPerDay = minutesPerHour * hoursPerDay;
 
 console.log(minutesPerDay)
+
+const secondsPerMinute = 60;
+
+const secondsPerDay = secondsPerMinute * minutesPerDay;
+
+console.log(secondsPerDay);
+
+const secondsPerWeek = secondsPerDay * 7;
+
+const secondsPerYear = secondsPerWeek * 52;
+console.log(secondsPerYear);
+
